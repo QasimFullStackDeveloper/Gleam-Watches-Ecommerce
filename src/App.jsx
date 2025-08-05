20f28a0068deec4fd9bb52ba00fdada77d0a80d4
unknown <sherazasmat435@gmail.com>
2025-08-05 17:14:56 +0500

    done the watches pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import './App.css';
 import AboutUs from './pages/aboutUs';
+import Watches from './pages/Watches';
 
 function App() {
   return (
-    <div>
-      <AboutUs />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<AboutUs />}/>
+         <Route path="/" element={<Watches />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
