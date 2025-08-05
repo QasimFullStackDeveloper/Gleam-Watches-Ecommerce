531d78f8845b595e34f39fa701daacf2347f375a
Syeda Hamd Atif Qazzafi <syedahamdatif3@gmail.com>
2025-08-05 22:19:07 +0500

    added cart and product page updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+<<<<<<< Updated upstream
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./pages/Checkout";
 import ContactUs from "./pages/ContactUs";
 import Products from './Products';
+=======
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Products from './Products';
+import Cart from './Cart';
+
+>>>>>>> Stashed changes
 function App() {
   return (
     <Router>
       <Routes>
+<<<<<<< Updated upstream
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/products" element={<Products />} />
+=======
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+>>>>>>> Stashed changes
       </Routes>
     </Router>
   );
